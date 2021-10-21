8ff0a3cec0e5c2feea9cbac86d49343d135810fe
meng_yuan <mengyuan@yuanxinjituan.com>
2021-10-21 15:26:14 +0800

    frame

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  title: 'SimpleBox',
+  themeConfig: {
+    smoothScroll: true,
+    logo: '/logo.png',
+    nav: [
+      { text: 'Home', link: '/' },
+      {
+        text: 'collection',
+        items: [
+          { text: 'ElementUI', link: 'https://element.faas.ele.me/#/zh-CN' },
+          { text: 'Vue', link: 'https://cn.vuejs.org/v2/guide/' }
+        ]
+      }
+    ]
+  },
+  
+}
